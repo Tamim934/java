f74e73f0d02f2f9668b2b4bb06e77ef5049993ab
Tamim934 <140682649+Tamim934@users.noreply.github.com>
2023-08-14 22:18:57 +0500

    practice

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log("hello");
+console.log("2+2");
+
+let text = 'java script is very hard';
+let textt = 'hellow world';
+let tamim = 'i like me';
+let number =10+10;
+let you = null;
+let hello = undefined;
+let name =100;
+console.log(name)
+ name ="good";
+ console.log(name)
+
+
+console.log(typeof hello)
+console.log(tamim);
+console.log(number);
+console.log(you);
